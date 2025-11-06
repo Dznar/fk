bcd630644f9d8fff14e8426f6fa9205c2da2dd45
Dznar <fakhri.mrabet@episousse.com.tn>
2025-11-06 17:35:18 +0100

    Update artifact path for Windows build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
+    base: './',
     target: ['es2021', 'chrome100', 'safari13'],
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
